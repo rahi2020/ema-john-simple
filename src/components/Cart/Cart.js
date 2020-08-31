146b22cb5271340b467197db79efb93915f72dbc
Razmik Khan Rahi <rahi.khan7303@gmail.com>
2020-08-31 19:49:41 +0600

    Done my ema-john website

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const Cart = (props) => {
     const cart = props.cart;
-    console.log(cart);
+    // console.log(cart);
     // const total = cart.reduce( (total, prd) => total + prd.price , 0);
     let total = 0;
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
-        total = total + product.price;
+        total = total + product.price * product.quantity;
+        debugger;
         
     }
     let shipping = 0;
@@ -31,12 +32,17 @@ const Cart = (props) => {
 
     return (
         <div>
-            <h4>Order Summary</h4>
-            <p>Items ordered: {cart.length} </p>
-            <p>Product Price: { formatNumber(total) }</p>
-            <p><small>shipping cost: {shipping}</small></p>
-            <p><small>Tax + VAT: {tax}</small></p>
-            <p>Total price: {grandTotal}</p>
+                <h4>Order Summary</h4>
+                <p>Items ordered: {cart.length} </p>
+                <p>Product Price: { formatNumber(total) }</p>
+                <p><small>shipping cost: {shipping}</small></p>
+                <p><small>Tax + VAT: {tax}</small></p>
+                <p>Total price: {grandTotal}</p>
+                <br/>
+                {
+                    props.children
+                }
+         
         </div>
     );
 };
